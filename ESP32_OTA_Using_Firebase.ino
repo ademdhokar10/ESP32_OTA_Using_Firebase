aafac0d68d526cd52800647893c47084b4beb933
ademdhokar10 <125218847+ademdhokar10@users.noreply.github.com>
2024-10-22 16:04:25 +0100

    Add files via upload

---
+++ ESP32_OTA_Using_Firebase.ino
@@ -0,0 +1,144 @@
+#include <LittleFS.h>
+#include <Arduino.h>
+#include <WiFi.h>
+
+#include <Firebase_ESP_Client.h>
+
+// Provide the token generation process info.
+#include <addons/TokenHelper.h>
+
+// Provide the SD card interfaces setting and mounting
+#include <addons/SDHelper.h>
+
+/* 1. Define the WiFi credentials */
+#define WIFI_SSID "2001"                    /* Your Wifi SSID */
+#define WIFI_PASSWORD "19821968"            /* Your Wifi Password */
+
+/* 2. Define the API Key */
+#define API_KEY "AIzaSyBIKU1XmpKhb2xiYK6cLTxijSSahaqRNiU"
+
+/* 3. Define the user Email and password that alreadey registerd or added in your project */
+#define USER_EMAIL "ehab4zu@gmail.com"      /* your gmail */
+#define USER_PASSWORD "123456"              /* your password */
+
+/* 4. Define the Firebase storage bucket ID e.g bucket-name.appspot.com */
+#define STORAGE_BUCKET_ID "tutorial-c161d.appspot.com"
+
+// Define Firebase Data object
+FirebaseData fbdo;
+
+FirebaseAuth auth;
+FirebaseConfig config;
+
+bool taskCompleted = false;
+
+void setup()
+{
+    Serial.begin(115200);
+    WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
+
+    Serial.print("Connecting to Wi-Fi");
+    unsigned long ms = millis();
+    while (WiFi.status() != WL_CONNECTED)
+    {
+        Serial.print(".");
+        delay(300);
+    }
+    Serial.println();
+    Serial.print("Connected with IP: ");
+    Serial.println(WiFi.localIP());
+    Serial.println();
+
+    Serial.printf("Firebase Client v%s\n\n", FIREBASE_CLIENT_VERSION);
+
+    /* Assign the api key (required) */
+    config.api_key = API_KEY;
+
+    /* Assign the user sign in credentials */
+    auth.user.email = USER_EMAIL;
+    auth.user.password = USER_PASSWORD;
+
+    // The WiFi credentials are required for Pico W
+    // due to it does not have reconnect feature.
+#if defined(ARDUINO_RASPBERRY_PI_PICO_W)
+    config.wifi.clearAP();
+    config.wifi.addAP(WIFI_SSID, WIFI_PASSWORD);
+#endif
+
+    /* Assign the callback function for the long running token generation task */
+    config.token_status_callback = tokenStatusCallback; // see addons/TokenHelper.h
+
+    // Comment or pass false value when WiFi reconnection will control by your code or third party library e.g. WiFiManager
+    Firebase.reconnectNetwork(true);
+
+    // Since v4.4.x, BearSSL engine was used, the SSL buffer need to be set.
+    // Large data transmission may require larger RX buffer, otherwise connection issue or data read time out can be occurred.
+    fbdo.setBSSLBufferSize(4096 /* Rx buffer size in bytes from 512 - 16384 */, 1024 /* Tx buffer size in bytes from 512 - 16384 */);
+
+    /* Assign download buffer size in byte */
+    // Data to be downloaded will read as multiple chunks with this size, to compromise between speed and memory used for buffering.
+    // The memory from external SRAM/PSRAM will not use in the TCP client internal rx buffer.
+    config.fcs.download_buffer_size = 2048;
+
+    Firebase.begin(&config, &auth);
+
+    // if use SD card, mount it.
+    //SD_Card_Mounting(); // See src/addons/SDHelper.h
+    pinMode(2, OUTPUT);
+}
+
+// The Firebase Storage download callback function
+void fcsDownloadCallback(FCS_DownloadStatusInfo info)
+{
+    if (info.status == firebase_fcs_download_status_init)
+    {
+        Serial.printf("Downloading file %s (%d) to %s\n", info.remoteFileName.c_str(), info.fileSize, info.localFileName.c_str());
+    }
+    else if (info.status == firebase_fcs_download_status_download)
+    {
+        Serial.printf("Downloaded %d%s, Elapsed time %d ms\n", (int)info.progress, "%", info.elapsedTime);
+    }
+    else if (info.status == firebase_fcs_download_status_complete)
+    {
+        Serial.println("Download completed\n");
+    }
+    else if (info.status == firebase_fcs_download_status_error)
+    {
+        Serial.printf("Download failed, %s\n", info.errorMsg.c_str());
+    }
+}
+
+void readFile(const char *path)
+{
+  File file = LittleFS.open(path, "r");
+  if(!file)
+  {
+    Serial.println("Failed to open file");
+    return;
+  }
+  Serial.println("Read from file:");
+  while(file.available())
+  {
+    Serial.print(file.read(), HEX);
+    Serial.print(' ');
+  }
+  file.close();
+}
+
+void loop()
+{
+
+    // Firebase.ready() should be called repeatedly to handle authentication tasks.
+    if (Firebase.ready() && !taskCompleted)
+    {
+        taskCompleted = true;
+
+        Serial.println("\nDownload file...\n");
+
+        // The file systems for flash and SD/SDMMC can be changed in FirebaseFS.h.
+        if (!Firebase.Storage.download(&fbdo, STORAGE_BUCKET_ID /* Firebase Storage bucket id */, "Application.bin" /* path of remote file stored in the bucket */, "/new.bin" /* path to local file */, mem_storage_type_flash /* memory storage type, mem_storage_type_flash and mem_storage_type_sd */, fcsDownloadCallback /* callback function */))
+            Serial.println(fbdo.errorReason());
+
+        readFile("/new.bin");
+    }
+}
